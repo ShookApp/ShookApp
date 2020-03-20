0de7b82c180cc8b3da2143fedac098b223b25a29
Samofan <dinter.florian@googlemail.com>
2020-03-20 17:02:15 +0100

    Add UserOverview.xaml

---
+++ ShookApp/Views/UserOverview.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShookApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserOverview : ContentPage
+    {
+        public UserOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
