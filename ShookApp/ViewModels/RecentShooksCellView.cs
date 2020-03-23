24d3461c3adb9faf349234d0f17fd6f8892b7011
Samofan <dinter.florian@googlemail.com>
2020-03-23 19:41:09 +0100

    Add Humanizer, Show last shooks depending on RecentShooksCellView

---
+++ ShookApp/ViewModels/RecentShooksCellView.cs
@@ -0,0 +1,41 @@
+﻿using Humanizer;
+using ShookModel.Models;
+using System;
+
+namespace ShookApp.ViewModels
+{
+    class RecentShooksCellView
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string End { get; private set; }
+        // TODO: If this Shook was won ? Color = green : Color = red
+        public string Color { get; private set; }
+
+        public RecentShooksCellView(Shook _shook)
+        {
+            Title = _shook.Title;
+            Description = _shook.Description;
+            End = CalculateEndTime(_shook.EndTime);
+            Color = "#fc85ae";
+        }
+
+        private string CalculateEndTime(DateTime endTime)
+        {
+            DateTime _returnDateTime = DateTime.Now;
+
+            if (endTime < _returnDateTime)
+            {
+                TimeSpan timeSpan = endTime.Subtract(_returnDateTime);
+
+                return "Ended " + timeSpan.Humanize() + " ago.";
+            }
+            else
+            {
+                TimeSpan timeSpan = _returnDateTime.Subtract(endTime);
+
+                return "Ending in " + timeSpan.Humanize() + ".";
+            }
+        }
+    }
+}
--- ShookApp/Views/UserOverview.xaml.cs
+++ ShookApp/Views/UserOverview.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ShookApp.ViewModels;
+using ShookModel.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,9 +24,33 @@ namespace ShookApp.Views
 
         private void BuildProfile()
         {
-            // TODO: Set the Picture which is coming from the LoginPackage as ProfilePicture
+            // TODO: Set the Picture which is coming from the LoginPackage as ProfilePicture.
             ProfilePictureView.Source = "profile_picture.png";
             UserNameLabel.Text = "Samofan";
+
+            // TODO: Get all Shooks of current user and add the 10 most recent to the list as 
+            // RecentShooksCellView Object.
+            recentShooksListView.ItemsSource = CreateListOfRecentShooksCellViews();
+        }
+
+        // Test.
+        private List<RecentShooksCellView> CreateListOfRecentShooksCellViews()
+        {
+            Shook shook1 = new Shook
+            {
+                Title = "Test",
+                Description = "Test Description",
+                EndTime = DateTime.Now
+            };
+
+            List<RecentShooksCellView> recentShooksCellViews = new List<RecentShooksCellView>();
+
+            recentShooksCellViews.Add(new RecentShooksCellView(shook1));
+            recentShooksCellViews.Add(new RecentShooksCellView(shook1));
+            recentShooksCellViews.Add(new RecentShooksCellView(shook1));
+            recentShooksCellViews.Add(new RecentShooksCellView(shook1));
+
+            return recentShooksCellViews;
         }
 
         #endregion
@@ -33,7 +59,7 @@ namespace ShookApp.Views
 
         private void OpenSettingsButton_Clicked(object sender, EventArgs e)
         {
-            // TODO: Open settings
+            // TODO: Open settings.
         }
 
         #endregion
