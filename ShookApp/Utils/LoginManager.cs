c039a5a55bdbed8dc0a62693cdba8184801e3cd8
Samofan <dinter.florian@googlemail.com>
2020-04-19 13:34:00 +0200

    Add login procedure

---
+++ ShookApp/Utils/LoginManager.cs
@@ -0,0 +1,70 @@
+﻿using MongoDB.Bson.Serialization;
+using RestSharp;
+using ShookModel.Models;
+using System;
+
+namespace ShookApp.Utils
+{
+    /// <summary>
+    /// This class handles the login action.
+    /// </summary>
+    class LoginManager
+    {
+        /// <summary>
+        /// Checks if the username and password are valid.
+        /// </summary>
+        /// <param name="username">Username of the user who wants to log in.</param>
+        /// <param name="password">Password of the user who wants to log in.</param>
+        /// <returns>True if the credentials are valid.</returns>
+        public bool CheckPassword(string username, string password)
+        {
+            var client = new ShookDebugHttpClient("https://IpOfShookREST/login?username=" + username + "&password=" + password);
+            var request = new RestRequest(Method.POST);
+            
+            IRestResponse response = client.Execute(request);
+
+            LoginPackage loginPackage;
+
+            try
+            {
+                loginPackage = BsonSerializer.Deserialize<LoginPackage>(response.Content);
+            }
+            catch(Exception)
+            {
+                //TODO: Exception handling (EndOfDocument)
+                throw new Exception();
+            }
+
+            return VerificateLoginPackage(loginPackage);
+        }
+
+        /// <summary>
+        /// Checks actually if the credentials are valid.
+        /// </summary>
+        /// <param name="loginPackage">The received <see cref="LoginPackage"/>.</param>
+        /// <returns>True if success of the <see cref="LoginPackage"/> is true.</returns>
+        private bool VerificateLoginPackage(LoginPackage loginPackage)
+        {
+            if(loginPackage.Success)
+            {
+                SaveLoginPackage(loginPackage);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Saves the <see cref="LoginPackage"/> and the apiKey to the <see cref="Statics"/> class.
+        /// </summary>
+        /// <param name="_loginPackage">The received <see cref="LoginPackage"/> with the valid user credentials.</param>
+        private void SaveLoginPackage(LoginPackage _loginPackage)
+        {
+            Statics.loginPackage = _loginPackage;
+            Statics.apiKey = _loginPackage.ApiKey;
+        }
+    }
+}
---
+++ ShookApp/Utils/Statics.cs
@@ -0,0 +1,25 @@
+﻿using ShookModel.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShookApp.Utils
+{
+    /// <summary>
+    /// This class holds some static variables which are used during runtime.
+    /// </summary>
+    class Statics
+    {
+        /// <summary>
+        /// The valid <see cref="LoginPackage"/> which is comming from
+        /// the <see cref="LoginManager"/>.
+        /// </summary>
+        public static LoginPackage loginPackage;
+
+        /// <summary>
+        /// The API key for ShookREST which is comming from
+        /// the <see cref="LoginManager"/>.
+        /// </summary>
+        public static string apiKey;
+    }
+}
